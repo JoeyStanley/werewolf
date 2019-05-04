41643de91a1dff0f5060dd9db3a266b728d043d6
Joey Stanley <joseph.abraham.stanley@gmail.com>
2019-05-04 03:17:27 -0400

    created the repository

---
+++ app.R
@@ -0,0 +1,38 @@
+
+library(shiny)
+library(tidyverse)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Werewolf Simulator"),
+  
+   mainPanel(
+     tabsetPanel(
+       tabPanel("Setup", 
+                plotOutput("plot")),
+       tabPanel("Game Mechanics", 
+                verbatimTextOutput("summary")),
+       tabPanel("Results", 
+                tableOutput("table"))
+     )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
